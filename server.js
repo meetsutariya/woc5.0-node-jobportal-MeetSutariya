6695a958b8b88af245d93706c44e7e3536490709
meetsutariya <75678696+meetsutariya@users.noreply.github.com>
2023-01-18 21:38:28 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const path = require('path')
+const morgan = require('morgan')
+const bodyparser = require('body-parser')
+const connectDB = require('./server/database/connection')
+
+const app = express();
+
+dotenv.config({path: 'config.env'})
+const PORT = process.env.PORT || 8080
+
+//log request
+app.use(morgan('tiny'));
+
+//mongoDB connection
+connectDB();
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended: true}))
+
+//set view engine
+app.set("view engine", "ejs")
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
+//load routers
+app.use('/', require('./server/routes/router'))
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+})
